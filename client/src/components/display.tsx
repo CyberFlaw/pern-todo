76b65a1b7f8a58b2949ccc4a06cf0d350fdf2e1e
CyberFlaw <53346567+CyberFlaw@users.noreply.github.com>
2021-05-22 16:01:37 +0530

    client website template added

---
+++ client/src/components/display.tsx
@@ -0,0 +1,26 @@
+import "./style.css";
+
+export default function ShowTodo() {
+  return (
+    <div className="display-port">
+      <div className="table">
+        <div className="table-header">
+          <div className="col1">ID</div>
+          <div className="col2">Description</div>
+          <div className="col3">Action</div>
+        </div>
+
+        <div className="table-content1">
+          <div className="col1">1</div>
+          <div className="col2">Bleep</div>
+          <div className="col3">Done</div>
+        </div>
+        <div className="table-content2">
+          <div className="col1">1</div>
+          <div className="col2">Lorem</div>
+          <div className="col3">Done</div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/search.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import ShowTodo from "./display";
+
+import "./style.css";
+
+export default function Search() {
+  const [value, setValue] = useState("");
+
+  const handleValueChange = (event: any) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className="text-input">
+        <input
+          className="input"
+          type="text"
+          value={value}
+          onChange={handleValueChange}
+        />
+        <button className="btn-add">
+          <img
+            src="https://img-premium.flaticon.com/png/512/271/271194.png?token=exp=1621626911~hmac=60aedac2e45c70055f0f6531a33c5430"
+            alt="add"
+          />
+        </button>
+        <button className="btn-search">
+          <img
+            src="https://img-premium.flaticon.com/png/512/598/598489.png?token=exp=1621626863~hmac=fb5224891304e370047d6bc0697e3260"
+            alt="search"
+          />
+        </button>
+      </div>
+      <ShowTodo />
+    </div>
+  );
+}
